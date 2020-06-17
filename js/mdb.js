1fc76b3508ed6ac355c5d8f35c69ce37a02eb609
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-06-17 07:29:05 +0700

    fixing chart

--- js/mdb.js
+++ js/mdb.js
@@ -12468,27 +12468,3 @@
     }).call(this, n(87)(t))
 }]);
 //# sourceMappingURL=mdb.min.js.map
-
-var ctx = document.getElementById("skill-horizontalBar").getContext('2d');
-var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-        {
-            label: "My First dataset",
-            data: [0, 65, 45, 65, 35, 65, 0],
-            backgroundColor: '#AD35BA',
-            borderColor: [
-            '#AD35BA',
-            ],
-            borderWidth: 2,
-            pointBorderColor: "#fff",
-            pointBackgroundColor: "rgba(173, 53, 186, 0.1)",
-        }
-        ]
-    },
-    options: {
-        responsive: true
-    }
-    });
