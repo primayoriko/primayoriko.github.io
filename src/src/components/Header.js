d7b30d9e08b6d2e8e540827422053ef8f93a9cff
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-12-26 15:53:36 +0700

    add Typing effect

--- src/src/components/Header.js
+++ src/src/components/Header.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 import ParticlesBg from "particles-bg";
+import TypeIt from "typeit-react";
 
 class Header extends Component {
-  render() {
+  typingDescription(description){
+     if(description == undefined)
+      return;
 
+     const desc = description.split("; ");
+     return (
+      <TypeIt 
+         element={"h1"}
+         style={{ color:"white", fontSize:"4.5vw" }}
+         options={{
+            // strings: ["This will be typed!", "sadasdas"],
+            speed: 120,
+            waitUntilVisible: true, 
+            loop: true
+          }}
+         getBeforeInit={instance => {
+            instance
+              .type(desc[0])
+              .pause(750)
+              .empty()
+              .type(desc[1])
+              .pause(750)
+              .empty()
+              .type(desc[2])
+              .pause(750)
+              .empty()
+              .type(desc[3])
+        
+            return instance;
+          }}
+      />
+     );
+  } 
+
+  render() {
     if(this.props.data){
         var project = this.props.data.project;
         var github = this.props.data.github;
@@ -17,7 +51,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-         <ParticlesBg type="circle" bg={true} />
+         <ParticlesBg color="#FFFFFF" type="cobweb" bg={true} num={45}/>
          <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
             <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -25,8 +59,12 @@ class Header extends Component {
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
-               <li><a className="smoothscroll" href="#resume">Resume</a></li>
-               <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+               <li><a className="smoothscroll" href="#education">Education</a></li>
+               <li><a className="smoothscroll" href="#experience">Experience</a></li>
+               <li><a className="smoothscroll" href="#achiviement">Achiviement</a></li>
+               <li><a className="smoothscroll" href="#certification">Certification</a></li>
+               <li><a className="smoothscroll" href="#skill">Skill</a></li>
+               <li><a className="smoothscroll" href="#project">Project</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
@@ -35,7 +73,8 @@ class Header extends Component {
          
             <div className="banner-text">
                <h1 className="responsive-headline">{name}</h1>
-               <h3>{description}.</h3>
+               { this.typingDescription(description) }
+
                <hr />
                <ul className="social">
                   <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
